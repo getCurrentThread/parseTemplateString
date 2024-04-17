fbcefbf0147ad9975ae9daba9ebfe53df18c1c92
getCurrentThread <wak8835@gmail.com>
2024-04-17 09:56:45 +0900

    Update parseTemplateString.js

--- parseTemplateString.js
+++ parseTemplateString.js
@@ -1,38 +1,8 @@
 function parseTemplateString(templateString, data) {
-  const regExp = /\${(.*?)}/g;
-  return templateString.replace(regExp, (match, expression) => {
-    let [expressionString, filters = ''] = expression.split('|');
-    const [propertyName, ...args] = expressionString.split(/\s*[(,]\s*/);
-    let propertyValue = data[propertyName.trim()];
-    if (propertyValue === undefined) {
-      return '';
-    }
-    if (args[0] !== '') {
-      for (let arg of args) {
-        if (arg.endsWith(')')) {
-          let [funcName, ...funcArgs] = arg.split(/\s*[(,]\s*/);
-          funcName = funcName.trim();
-          funcArgs = funcArgs.map(arg => {
-            if (arg.startsWith('"') && arg.endsWith('"')) {
-              return arg.slice(1, -1);
-            } else if (arg in data) {
-              return data[arg];
-            } else {
-              return arg;
-            }
-          });
-          const func = data[funcName] || propertyValue[funcName];
-          if (func !== undefined) {
-            propertyValue = func.apply(null, [propertyValue, ...funcArgs]);
-          }
-        } else {
-          const filter = data[args[0]] || propertyValue[args[0]];
-          if (filter !== undefined) {
-            propertyValue = filter.apply(null, [propertyValue]);
-          }
-        }
-      }
-    }
-    return propertyValue;
-  });
+   return interpolate(t, c);
+}
+
+// https://github.com/catpea/cataclysm/blob/master/api/processor.mjs#L604C1-L606C2
+function interpolate(t, c){
+  return t.replace(/\${([^}]+)}/g,(m,p)=>p.split('.').reduce((a,f)=>a?a[f]:undefined,c)??m);
 }
